766434176116708da33ed81f4aeb92a4f7d8df18
michol.lee <lovethefeel@gmail.com>
2025-08-01 21:09:44 +0900

    init

---
+++ src/main/java/io/codelee/webflux/WebfluxExerciseApplication.java
@@ -0,0 +1,13 @@
+package io.codelee.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxExerciseApplication.class, args);
+    }
+
+}
